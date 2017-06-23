e682d226140bffd34f4ae5f51577b77db8ccce33
Kristian Cabading <kcabading@crmonline.com.au>
2017-06-23 21:37:49 +0800

    Initial push

---
+++ server/main.js
@@ -0,0 +1,12 @@
+var Express = require('express');
+
+var app = new Express();
+//asdfasdf
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('./../app/index',{});
+})
+.use(Express.static(__dirname + '/../.tmp'))
+.listen(7777);
